e2b773788c5c7dbb167541655a21511c66764a7b
Emrullah Ayın <ayinemrullah@gmail.com>
2019-06-11 17:52:21 +0300

    added ui components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+import "normalize.css";
+import "./App.scss";
+
+const columnsStyle = {
+  transition: "all .5s ease-in-out",
+  columnGap: "30px",
+  columnFll: "initial"
+};
+
+const figureStyle = {
+  marginBottom: "30px",
+  display: "inline-block",
+  verticalAlign: "top"
+};
+
+const imageStyle = {
+  transition: "all .5s ease-in-out",
+  backfaceVisibility: "hidden",
+  maxWidth: "100%",
+  verticalAlign: "middle"
+};
+
+const App = () => {
+  return (
+    <div className="columns" style={columnsStyle}>
+      <figure style={figureStyle}>
+        <img
+          style={imageStyle}
+          src="https://bulma.io/images/placeholders/1280x960.png"
+          alt="Placeholder image"
+        />
+      </figure>
+    </div>
+  );
+};
 
 export default App;
